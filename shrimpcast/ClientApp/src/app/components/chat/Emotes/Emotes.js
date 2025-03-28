6e68a5c1ce6c0ab90205e6b696d38cc896bd2ee1
l29utp0 <info@ptchan.org>
2025-03-28 02:32:36 +0000

    try new emote box

--- shrimpcast/ClientApp/src/app/components/chat/Emotes/Emotes.js
+++ shrimpcast/ClientApp/src/app/components/chat/Emotes/Emotes.js
@@ -1,50 +1,85 @@
-import * as React from "react";
+import React from "react";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
-import { Avatar, Box, IconButton, Paper } from "@mui/material";
+import { Avatar, Box, IconButton, Paper, Tooltip } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
-const EmotesWrapperSx = {
-    position: "absolute",
-    bottom: "65px",
-    width: "100%",
-    bgcolor: "primary.900",
-    zIndex: 1,
+// Styled components
+const EmotesWrapper = styled(Paper)(({ theme }) => ({
+  position: "absolute",
+  bottom: "65px",
+  width: "100%",
+  backgroundColor: theme.palette.primary[900],
+  zIndex: 1,
+}));
+
+const EmotesGrid = styled(Box)(({ theme }) => ({
+  marginTop: "3px",
+  maxHeight: "200px",
+  padding: "7.5px",
+  overflowY: "auto",
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fill, minmax(40px, 1fr))",
+  gap: "5px",
+
+  // Custom scrollbar styling
+  "&::-webkit-scrollbar": {
+    width: "8px",
   },
-  EmotesSx = {
-    marginTop: "3px",
-    maxHeight: "200px",
-    bgcolor: "primary.900",
-    padding: "7.5px",
-    overflowY: "scroll",
-  };
+  "&::-webkit-scrollbar-track": {
+    background: theme.palette.primary[800],
+  },
+  "&::-webkit-scrollbar-thumb": {
+    background: theme.palette.secondary.main,
+    borderRadius: "4px",
+  },
+}));
 
-const Emotes = (props) => {
-  const handleClose = () => props.setEmotes(false),
-    emoteClick = (emote) => {
-      props.setMessage((message) => message + emote);
-      handleClose();
-      // Focus the chat input after adding the emote
-      if (props.inputRef?.current) {
-        props.inputRef.current.focus();
-      }
-    };
+const EmoteButton = styled(IconButton)(({ theme }) => ({
+  padding: "4px",
+  transition: "transform 0.2s ease",
+  "&:hover": {
+    transform: "scale(1.1)",
+  },
+}));
+
+const EmoteAvatar = styled(Avatar)({
+  borderRadius: 0,
+  width: "30px",
+  height: "30px",
+});
+
+const Emotes = ({ emotes, setMessage, inputRef, setEmotes }) => {
+  const handleClose = () => setEmotes(false);
+
+  const handleEmoteClick = (emote) => {
+    setMessage((prevMessage) => prevMessage + emote);
+    handleClose();
+
+    // Focus input after adding emote
+    if (inputRef?.current) {
+      inputRef.current.focus();
+    }
+  };
 
   return (
     <ClickAwayListener onClickAway={handleClose}>
-      <Paper sx={EmotesWrapperSx} elevation={2}>
-        <Box sx={EmotesSx}>
-          {props.emotes.map((emote) => (
-            <IconButton onClick={() => emoteClick(emote.name)} key={emote.name}>
-              <Avatar
-                alt={emote.alt}
-                sx={{ borderRadius: "0px", width: "30px", height: "30px" }}
-                src={emote.url}
-              />
-            </IconButton>
+      <EmotesWrapper elevation={2}>
+        <EmotesGrid className="scrollbar-custom">
+          {emotes.map((emote) => (
+            <Tooltip key={emote.name} title={emote.name} placement="top">
+              <EmoteButton onClick={() => handleEmoteClick(emote.name)}>
+                <EmoteAvatar
+                  alt={emote.alt || emote.name}
+                  src={emote.url}
+                  variant="square"
+                />
+              </EmoteButton>
+            </Tooltip>
           ))}
-        </Box>
-      </Paper>
+        </EmotesGrid>
+      </EmotesWrapper>
     </ClickAwayListener>
   );
 };
 
-export default Emotes;
+export default React.memo(Emotes);
